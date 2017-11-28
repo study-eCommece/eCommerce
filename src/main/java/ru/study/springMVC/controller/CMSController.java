14af837640ac0e4bb35dd70fe5f29473f9696f78
Egor <gvozdetscky.egor@yandex.ru>
2017-11-28 18:36:31 +0400

    Алилуя, все работает (на самом деле нет, абслтн пути ненстрн)

--- src/main/java/ru/study/springMVC/controller/CMSController.java
+++ src/main/java/ru/study/springMVC/controller/CMSController.java
@@ -1,37 +1,24 @@
 package ru.study.springMVC.controller;
 
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.databind.util.JSONPObject;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.Resource;
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import ru.study.springMVC.model.JsonObject;
 
 import java.io.*;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class CMSController {
 
 	@RequestMapping("api/cmi/test")
-	public List<JsonObject> getTest() {
+	public List<JsonObject> getTest() throws FileNotFoundException {
 
-		List<JsonObject> jsonObjectList = null;
-
-		try {
-			ObjectInputStream ois = new ObjectInputStream(new FileInputStream("D:\\java\\IProject\\tgu\\eCommerce\\src\\main\\webapp\\resources\\json\\security.json"));
-			jsonObjectList = (List<JsonObject>) ois.readObject();
-			System.out.println(jsonObjectList.size());
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		Gson gson = new Gson();
+		JsonReader reader = new JsonReader(new FileReader("D:\\java\\IProject\\tgu\\eCommerce\\src\\main\\webapp\\resources\\json\\security.json"));
+		List<JsonObject> jsonObjectList = Arrays.asList(gson.fromJson(reader, JsonObject[].class)); // contains the whole reviews list
 
 		return jsonObjectList;
 	}
