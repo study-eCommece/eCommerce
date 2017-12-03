ce9fe733654db701937ccd4c7047674e77922d9d
Egor <gvozdetscky.egor@yandex.ru>
2017-12-03 15:15:02 +0400

    md5 заменен на hashCode

--- src/main/java/ru/study/springMVC/util/CryptPassword.java
+++ src/main/java/ru/study/springMVC/util/CryptPassword.java
@@ -5,6 +5,6 @@ import org.springframework.util.DigestUtils;
 public class CryptPassword {
 
 	public static String encode(String password) {
-		return new String(DigestUtils.md5Digest(password.getBytes()));
+		return String.valueOf(password.hashCode());
 	}
 }
