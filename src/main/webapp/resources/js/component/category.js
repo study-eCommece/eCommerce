16b2eca05ebac6f11ffa7ae79b8e57a382165265
ovchin <oB4uH132@gmail.com>
2017-12-22 11:38:39 +0400

    Отображение категорий AJAX

--- src/main/webapp/resources/js/component/category.js
+++ src/main/webapp/resources/js/component/category.js
@@ -19,12 +19,70 @@ $(document).ready(function () {
     }
 
     function fillCategory(childCategoryList) {
-        var content="";
+
+        $('.category-list').html("");
+
         childCategoryList.forEach(function(childCategory) {
-            content += '<li><a href="#">' + childCategory.name + '</a></li>';
+            var content = '<li><a id="' + childCategory.id + '" role="button">' + childCategory.name + '</a></li>';
+
+            $('.category-list').append(content);
+
+            $("#" + childCategory.id).click(function () {
+
+                getProduct(childCategory.id);
+            });
         });
 
-        $('.category-list').html(content);
+
+    }
+
+    function getProduct(id) {
+
+        $.ajax({
+            type: "GET",
+            url: 'api/productByCategory?categoryId=' + id,
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            cache: false,
+            success: function (productList) {
+                showProduct(productList);
+
+            },
+            error: function (e) {
+                alert(e.responseText);
+            }
+        });
+    }
+
+    function showProduct(productList) {
+
+
+
+        $('#products').html("");
+
+        productList.forEach(function (product) {
+
+            var contentProduct = '<div class="item col-xs-4 col-lg-4">\n' +
+                ' <div class="thumbnail">\n' +
+                ' <img class="group list-group-image" src="http://placehold.it/400x250/000/fff" alt="" />\n' +
+                ' <div class="caption">\n' +
+                ' <h4 class="group inner list-group-item-heading">\n' +
+                product.name + '</h4>\n' +
+                ' <p class="group inner list-group-item-text"></p>\n' +
+                ' <div class="row">\n' +
+                ' <div class="col-xs-12 col-md-6">\n' +
+                ' <p class="lead">$' + product.price + '</p>\n' +
+                ' </div>\n' +
+                ' <div class="col-xs-12 col-md-6">\n' +
+                ' <a class="btn btn-success" href="#">Add to cart</a>\n' +
+                ' </div>\n' +
+                ' </div>\n' +
+                ' </div>\n' +
+                ' </div>\n' +
+                ' </div>';
+
+            $('#products').append(contentProduct);
+        })
     }
 
 });
